ee9792417a52473fbc04def045733ae988d34194
ssohy <109409651+ssohy@users.noreply.github.com>
2024-01-24 11:48:03 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+int main()
+{
+    int N = 0, cnt = 0;
+    cin >> N;
+    for(int i = 5; i <= N; i *= 5)
+        cnt += (N/i);
+    cout << cnt << endl;
+    return 0;
+}
