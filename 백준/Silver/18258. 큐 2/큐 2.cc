5dfb77b3c0712c5552e0839702663af213742faf
ssohy <109409651+ssohy@users.noreply.github.com>
2024-01-29 17:14:37 +0900

    [Silver IV] Title: 큐 2, Time: 384 ms, Memory: 10424 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.cc
+++ 백준/Silver/18258. 큐 2/큐 2.cc
@@ -2,54 +2,52 @@
 #include <queue>
 using namespace std;
 
-
-int N;
-int main(void) {
+int main()
+{
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-
-	queue<int> myqueue;
-	string comm;
-
-
-	cin >> N;
-	for (int i = 0; i < N; i++) {
-		cin >> comm;
-		if (comm == "push") {
-			int num;
-			cin >> num;
-			myqueue.push(num);
-		}
-		else if (comm == "pop") {
-			if (myqueue.empty()) {
-				cout << -1 << '\n';
-			}
-			else {
-				cout << myqueue.front() << '\n';
-				myqueue.pop();
-			}
-		}
-		else if (comm == "size") {
-			cout << myqueue.size() << '\n';
-		}
-		else if (comm == "empty") {
-			cout << (int)myqueue.empty() << '\n';
-		}
-		else if (comm == "front") {
-			if (myqueue.empty()) {
-				cout << -1 << '\n';
-			}
-			else {
-				cout << myqueue.front() << '\n';
-			}
-		}
-		else if (comm == "back") {
-			if (myqueue.empty()) {
-				cout << -1 << '\n';
-			}
-			else {
-				cout << myqueue.back() << '\n';
-			}
-		}
-	}
+    int N = 0, x = 0;
+    string cmd = "";
+    queue<int> q;
+    
+    cin >> N;
+    
+    for (int i = 0; i < N; i++){
+        cin >> cmd;
+        if (cmd == "push") {
+            cin >> x;
+            q.push(x);
+        } 
+        else if (cmd == "pop") {
+            if(q.empty()) 
+            	cout << -1 << '\n';
+            else {
+				cout << q.front() << '\n';
+				q.pop();
+            }
+        } 
+        else if (cmd == "size") {
+            cout << q.size() << '\n';
+        } 
+        else if (cmd == "empty") {
+            if(q.empty()) 
+            	cout << 1 << '\n';
+            else 
+            	cout << 0 << '\n';
+        } 
+        else if (cmd == "front") {
+            if(q.empty()) 
+            	cout << -1 << '\n';
+            else 
+            	cout << q.front() << '\n';
+        } 
+        else if(cmd == "back"){
+            if(q.empty()) 
+            	cout << -1 << '\n';
+            else 
+            	cout << q.back() << '\n';
+        }
+    }
+    
+    return 0;
 }
