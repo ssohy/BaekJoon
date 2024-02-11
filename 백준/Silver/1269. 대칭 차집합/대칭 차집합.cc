e7f7ff1944fe18d39f229b9cd5152eaee8697623
ssohy <109409651+ssohy@users.noreply.github.com>
2024-02-11 15:18:15 +0900

    [Silver IV] Title: 대칭 차집합, Time: 68 ms, Memory: 5160 KB -BaekjoonHub

---
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int symDifOfSet(vector<int> x, vector<int> y){
+    sort(x.begin(), x.end());
+    sort(y.begin(), y.end());
+
+    int cnt = 0;
+    int i = 0, j = 0;
+    
+    while (i < x.size() && j < y.size()) {
+        if (x[i] < y[j]) {
+            cnt++;
+            i++;
+        } else if (x[i] > y[j]) {
+            cnt++;
+            j++;
+        } else {
+            i++;
+            j++;
+        }
+    }
+    cnt += x.size() - i;
+    cnt += y.size() - j;
+    return cnt;
+}
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int A = 0, B = 0, result = 0;
+    cin >> A >> B;
+    vector<int> a(A), b(B);
+    for(int i = 0; i < A; i++)
+        cin >> a[i];
+    for(int i = 0; i < B; i++)
+        cin >> b[i];
+    result = symDifOfSet(a, b);
+    cout << result << '\n';
+    return 0;
+}
