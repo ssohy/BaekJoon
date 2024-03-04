d91bce027a01b787a2fffae7c4a608a24e34fc3d
ssohy <109409651+ssohy@users.noreply.github.com>
2024-03-05 00:05:38 +0900

    [Silver V] Title: 나이순 정렬, Time: 112 ms, Memory: 9732 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+
+using namespace std;
+
+bool comp(pair<int, string>a, pair<int, string>b)
+//	이는 사용자 정의 정렬 기준이다. 첫번째 원소를 기준으로 오름차순.
+{
+	return a.first < b.first;
+}
+int main(void)
+{
+	int n;
+	cin >> n;
+
+	pair<int, string>mem_cop;
+	vector<pair<int, string>>mem;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> mem_cop.first >> mem_cop.second;
+		mem.push_back(mem_cop);
+	}
+	
+	stable_sort(mem.begin(), mem.end(), comp);
+	
+	for (int i = 0; i < mem.size(); i++)
+		cout << mem.at(i).first << ' ' << mem.at(i).second << '\n';
+	
+	return 0;
+}
