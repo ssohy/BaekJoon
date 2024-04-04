09ea29ceb77199d9bd6051935051a7f4e206d4c1
ssohy <109409651+ssohy@users.noreply.github.com>
2024-04-04 22:29:11 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N = 0;
+    cin >> N;
+    
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < i + 1; j++){
+            cout << "*";
+        }
+        cout << '\n';
+    }
+    
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N - i - 1; j++){
+            cout << "*";
+        }
+        cout << '\n';
+    }
+    
+    return 0;
+}
