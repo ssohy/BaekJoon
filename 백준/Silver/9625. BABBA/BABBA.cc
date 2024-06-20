a7feae3c7f3fb9710de35ae1f655c695384a554b
ssohy <109409651+ssohy@users.noreply.github.com>
2024-06-20 17:55:18 +0900

    [Silver V] Title: BABBA, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9625. BABBA/BABBA.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int K = 0;
+	long long A[45];
+	long long B[45];
+
+	A[0] = 0;
+	B[0] = 1;
+	A[1] = 1;
+	B[1] = 1;
+
+	cin >> K;
+	
+	for (int i = 2; i < K; i++) {
+		A[i] = A[i - 1] + A[i - 2];
+		B[i] = B[i - 1] + B[i - 2];
+	}
+
+	cout << A[K - 1] << " " << B[K - 1];
+
+	return 0;
+}
