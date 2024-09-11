73b74a9e51c6c7461952d71e52cb691b77d46494
ssohy <109409651+ssohy@users.noreply.github.com>
2024-09-12 00:23:03 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string str;
+
+	while(true) {
+		getline(cin, str);
+		if(str == "") break;
+
+		cout << str << '\n';
+	}
+
+    return 0;
+}
