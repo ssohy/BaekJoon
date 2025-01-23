c0cbfde05e0b186df5587d459cff2c2b8aa1bd57
ssohy <109409651+ssohy@users.noreply.github.com>
2025-01-23 18:56:44 +0900

    [Silver IV] Title: 30, Time: 8 ms, Memory: 3044 KB -BaekjoonHub

--- 백준/Silver/10610. 30/30.cc
+++ 백준/Silver/10610. 30/30.cc
@@ -9,21 +9,21 @@ int main()
     string N = "";
     int sum = 0;
     vector <int> num;
+    
     cin >> N;
 
     for(int i = 0; i < N.length(); i++){
         sum += (N[i] - '0');
         num.push_back(N[i] - '0');
     }
+    
     sort(num.rbegin(), num.rend());
-    if((sum % 3 == 0) && (num[num.size()-1] == 0)){
-        for(int i = 0; i < num.size(); i++){
+    
+    if((sum % 3 == 0) && (num[num.size()-1] == 0))
+        for(int i = 0; i < num.size(); i++)
             cout << num[i];
-        }
-    }
-    else{
+    else
         cout << -1;
-    }
     
     return 0;
 }
