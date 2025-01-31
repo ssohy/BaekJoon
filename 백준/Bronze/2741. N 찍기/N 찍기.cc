32e04d4ecb6d577ae62a83ebee60b1986ba3a0ec
ssohy <109409651+ssohy@users.noreply.github.com>
2025-01-31 23:45:25 +0900

    [Bronze V] Title: N 찍기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int N = 0;
+    cin >> N;
+    for(int i = 0; i < N; i++)
+        cout << i + 1 << "\n";
+
+    return 0;
+}
