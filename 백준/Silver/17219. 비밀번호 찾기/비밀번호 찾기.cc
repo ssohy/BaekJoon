0de52f4c7591168a657a36c56ab8e93cf162743f
ssohy <109409651+ssohy@users.noreply.github.com>
2025-03-31 21:29:47 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 96 ms, Memory: 16448 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.cc
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.cc
@@ -4,20 +4,22 @@ using namespace std;
 
 int main() 
 {
-    int N = 0, M = 0;
-    string site, pwd;
-    unordered_map<string, string> siteNpwd;
+   ios::sync_with_stdio(0);
+  cin.tie(0); cout.tie(0);
+  int N = 0, M = 0;
+  string site = "", pwd = "";
+  unordered_map<string, string> siteNpwd;
 
-    cin >> N >> M;
-    while (N--) {
-        cin >> site >> pwd;
-        siteNpwd[site] = pwd;
-    }
+  cin >> N >> M;
+  while (N--) {
+      cin >> site >> pwd;
+      siteNpwd[site] = pwd;
+  }
 
-    while (M--) {
-        cin >> site;
-        cout << siteNpwd[site] << '\n';
-    }
+  while (M--) {
+      cin >> site;
+      cout << siteNpwd[site] << '\n';
+  }
 
-    return 0;
+  return 0;
 }
