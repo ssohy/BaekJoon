d8efe925e8c97b83472a74fc1859df88c0b0ae4f
ssohy <109409651+ssohy@users.noreply.github.com>
2025-04-09 15:50:00 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 88 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() 
+{
+    string str = "";
+    stack<char> bracket;
+    getline(cin, str);
+
+    while (str != ".") {
+        bool isBalanced = true;
+        for (int i = 0; i < str.length(); i++) {
+            if (str[i] == '(' || str[i] == '[')
+                bracket.push(str[i]);
+
+            else if (str[i] == ')') {
+                if (!bracket.empty() && bracket.top() == '(')
+                    bracket.pop();
+                else {
+                    isBalanced = false;
+                    break;
+                }
+            }
+
+            else if (str[i] == ']') {
+                if (!bracket.empty() && bracket.top() == '[')
+                    bracket.pop();
+                else {
+                    isBalanced = false;
+                    break;
+                }
+            }
+        }
+
+        if (isBalanced && bracket.empty())
+            cout << "yes" << '\n';
+        else
+            cout << "no" << '\n';
+
+        while (!bracket.empty()) bracket.pop();
+        getline(cin, str);
+    }
+
+    return 0;
+}
