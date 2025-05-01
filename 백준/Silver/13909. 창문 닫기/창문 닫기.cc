699567080e2cadc28ff07999f8d1b6ca5605e97a
ssohy <109409651+ssohy@users.noreply.github.com>
2025-05-01 18:00:50 +0900

    [Silver V] Title: 창문 닫기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() 
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    long long N;
+    cin >> N;
+    cout << (long long)sqrt(N) << '\n';
+    return 0;
+}
