bdddb96df64e8cba8e522f578098ee2f4967537a
ssohy <109409651+ssohy@users.noreply.github.com>
2025-06-02 16:28:13 +0900

    [Silver V] Title: 수 이어가기, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2635. 수 이어가기/수 이어가기.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() 
+{
+    int n, maxLen = 0;
+    vector<int> maxArr;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        vector<int> arr;
+        arr.push_back(n);
+        arr.push_back(i);
+
+        while (true) {
+            int next = arr[arr.size() - 2] - arr[arr.size() - 1];
+            if (next < 0) break;
+            arr.push_back(next);
+        }
+
+        if (arr.size() > maxLen) {
+            maxLen = arr.size();
+            maxArr = arr;
+        }
+    }
+
+    cout << maxLen << '\n';
+    for (size_t i = 0; i < maxArr.size(); i++) {
+        cout << maxArr[i];
+        if (i != maxArr.size() - 1) cout << ' ';
+    }
+    return 0;
+}
