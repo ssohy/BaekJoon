39b4a439a979b024cfa360d63266c255d6616d98
ssohy <109409651+ssohy@users.noreply.github.com>
2025-06-04 20:03:19 +0900

    [Silver V] Title: 무한 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/12871. 무한 문자열/무한 문자열.cc
+++ 백준/Silver/12871. 무한 문자열/무한 문자열.cc
@@ -1,35 +1,19 @@
 #include <iostream>
+#include <numeric>
 using namespace std;
 
-// 최대공약수
-int gcd(int a, int b){
-    while(b != 0){
-        int tmp = a % b;
-        a = b;
-        b = tmp;
-    }
-    return a;
+bool f(string& str1, string& str2){
+  int len = lcm(str1.length(), str2.length());
+  for(int i = 0; i < len; i++){
+    if(str1[i % str1.length()] != str2[i % str2.length()])
+      return false;
+  }
+  return true;
 }
 
-// 최소공배수
-int lcm(int a, int b){
-    return a * b / gcd(a, b);
-}
-
-bool f(const string& str1, const string& str2){
-    int len1 = str1.size();
-    int len2 = str2.size();
-    int len = lcm(len1, len2);  // 최소공배수 길이까지 반복 비교
-
-    for(int i = 0; i < len; i++){
-        if(str1[i % len1] != str2[i % len2])
-            return false;
-    }
-    return true;
-}
-
-int main() {
-    string s, t;
+int main() 
+{
+    string s = "", t = "";
     cin >> s >> t;
     cout << f(s, t);
     return 0;
