3525e7a4343405b4a564aa6c6f984a3b5d2e8930
ssohy <109409651+ssohy@users.noreply.github.com>
2025-07-12 03:35:15 +0900

    [Bronze V] Title: 검증수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num, answer = 0;
+	int square = 0;
+
+	for (int i = 0; i < 5; i++) {
+		cin >> num;
+
+		square += num * num;
+	}
+	answer = square % 10;
+
+	cout << answer;
+}
