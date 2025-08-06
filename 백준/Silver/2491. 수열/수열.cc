df0ca4d8e90eab6ae7893e10ba43b032986c36e7
ssohy <109409651+ssohy@users.noreply.github.com>
2025-08-06 18:07:56 +0900

    [Silver IV] Title: 수열, Time: 20 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2491. 수열/수열.cc
+++ 백준/Silver/2491. 수열/수열.cc
@@ -4,41 +4,38 @@ using namespace std;
 
 int main() 
 {
-    int N = 0, maxcnt = 1, cnt = 1, maxcnt2 = 1;
+    int N = 0, maxcnt = 1, up = 1, maxcnt2 = 1, down = 1;
     cin >> N;
     vector<int> arr(N);
     for(int i = 0; i < N; i++){
       cin >> arr[i];
     }
-    for(int i = 0; i < N; i++){ // 오름차순 체크
-      if(i <= N - 2){
+    for(int i = 0; i < N; i++){ 
+      if(i <= N - 2){ // 오름차순 체크
         if(arr[i] <= arr[i+1]){
-          cnt++;
+          up++;
         }
         else{
-          if(maxcnt < cnt)
-            maxcnt = cnt;
-          cnt = 1;
+          if(maxcnt < up)
+            maxcnt = up;
+          up = 1;
         }
       }
-    }
-    if(maxcnt < cnt)
-      maxcnt = cnt;
-    cnt = 1;
-    for(int i = 0; i < N; i++){ // 내림차순 체크
-      if(i <= N - 2){
+      
+      if(i <= N - 2){ // 내름차순 체크
         if(arr[i] >= arr[i+1])
-          cnt++;
+          down++;
         else{
-          if(maxcnt2 < cnt)
-            maxcnt2 = cnt;
-          cnt = 1;
+          if(maxcnt2 < down)
+            maxcnt2 = down;
+          down = 1;
         }
       }
     }
-    if(maxcnt2 < cnt)
-      maxcnt2 = cnt;
-
+    if(maxcnt < up)
+      maxcnt = up;
+    if(maxcnt2 < down)
+      maxcnt2 = down;
     cout << (maxcnt > maxcnt2 ? maxcnt: maxcnt2) << '\n';
     return 0;
 }
