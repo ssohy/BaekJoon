d77480365f26d53efbbe90cd391f853c64504683
ssohy <109409651+ssohy@users.noreply.github.com>
2025-08-20 17:25:54 +0900

    [Silver III] Title: N과 M (3), Time: 396 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int N, M;
+vector<int> arr;
+void dfs()
+{
+  if(arr.size() == M){
+    for(int i = 0; i < M; i++){
+      cout << arr[i] << (i == M - 1 ? '\n' : ' ');
+    }
+    return;
+  }
+  
+  for(int i = 1; i <= N; i++){
+    arr.push_back(i);
+    dfs();
+    arr.pop_back();
+  }
+  
+}
+
+
+int main() 
+{
+    cin >> N >> M;
+    dfs();
+    return 0;
+}
