6af037524b283b6352f1982aae966e02062bce3e
ssohy <109409651+ssohy@users.noreply.github.com>
2025-10-15 23:09:38 +0900

    [Silver IV] Title: GCD 합, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9613. GCD 합/GCD 합.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+using namespace std;
+
+int main() 
+{
+    int t = 0, n = 0;
+    cin >> t;
+    while(t--){
+      cin >> n;
+      vector<long long> arr(n);
+      long long sum = 0;
+      for(int i = 0; i < n; i++){
+        cin >> arr[i];
+      }
+      for(int i = 0; i < n; i++){
+        for(int j = i + 1; j < n; j++){
+          sum += gcd(arr[i], arr[j]);
+        }
+      }
+      cout << sum << '\n';
+    }
+    return 0;
+}
